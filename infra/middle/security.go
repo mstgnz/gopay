e627e706869189e398f69f8cb4509d6c68c5b916
Mesut GENEZ <genezmesut@gmail.com>
2025-07-02 18:50:28 +0300

    cors

--- infra/middle/security.go
+++ infra/middle/security.go
@@ -17,7 +17,7 @@ func SecurityHeadersMiddleware() func(http.Handler) http.Handler {
 			w.Header().Set("X-Frame-Options", "DENY")
 			w.Header().Set("X-XSS-Protection", "1; mode=block")
 			w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
-			w.Header().Set("Content-Security-Policy", "default-src 'self'")
+			w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com")
 			w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
 
 			next.ServeHTTP(w, r)
